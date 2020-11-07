a21dbd10a452672348725b1d0ecc8d2c8845c8fe
ArchitTheCoder <72322982+ArchitTheCoder@users.noreply.github.com>
2020-11-07 14:50:21 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+var divisions = [];
+var plinkos = [];
+var particles = [];
+
+var divisionHeight = 300;
+
+
+
+function setup() {
+  createCanvas(480, 800);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(240, height, 480, 20)
+
+
+
+
+
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+
+  //Plinkos
+  for (var j = 40; j <= width; j = j + 50) {
+    plinkos.push(new Plinko(j, 75, 20))
+  }
+  for (var j = 15; j <= width; j = j + 50) {
+    plinkos.push(new Plinko(j, 175, 20))
+  }
+  for (var j = -10; j <= width; j = j + 50) {
+    plinkos.push(new Plinko(j, 275, 20))
+  }
+  for (var j = -35; j <= width; j = j + 50) {
+    plinkos.push(new Plinko(j, 375, 20))
+  }
+  for (var j = 0; j < plinkos.length; j++) {
+    plinkos[j].display();
+  }
+
+  //Particles
+  if (frameCount % 90 === 0) {
+    particles.push(new Particle(random(width / 2 - 10, width / 2 + 10), 10, 10))
+  }
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].display();
+  }
+
+  //Divisions
+  for (var k = 0; k <= width; k = k + 80) {
+    divisions.push(new Divisions(k, height - divisionHeight / 2, 10, divisionHeight));
+  }
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  ground.display();
+}
+
